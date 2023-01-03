a1c7baa507e34c5c5f7fa195be04e7520a1588c8
Andres Maneiro <amaneiro7@gmail.com>
2023-01-03 06:42:59 -0400

    clase de css con react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
+const todos = [
+  {
+    id: 1,
+    text: 'Cortar cebolla',
+    compleetd: false,
+  },
+  {
+    id: 2,
+    text: 'Tomar el curso de React',
+    compleetd: false,
+  },
+  {
+    id: 3,
+    text: 'Armar la base de datos para la pag de ejercicio',
+    compleetd: false,
+  },
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter />
+      <TodoSearch />
+      
+      <TodoList >
+        {todos.map((todo) => (
+          <TodoItem key={todo.id } text={todo.text}/>
+        ))}
+      </TodoList >
+      <CreateTodoButton />      
+    </>
   );
 }
 
---
+++ src/TodoSearch.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style/TodoSearch.css'
+
+export function TodoSearch() {
+    return(
+        <input className='TodoSearch' placeholder="Cebolla" />
+    );
+};
